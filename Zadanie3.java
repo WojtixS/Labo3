390e0729fe3d6d4b8f32a76927ba72f50a6fbbaa
WojtixS <91822459+WojtixS@users.noreply.github.com>
2021-10-17 09:42:08 +0200

    Add files via upload

---
+++ Zadanie3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zadanie3;
+
+/**
+ *
+ * @author w65143
+ */
+public class Zadanie3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  String[] slowa = { "Wojtek", "Student" };
+    for (String slowo : slowa) {
+      System.out.print(slowo.toUpperCase() + " ");
+    }
+    }
+}
+
